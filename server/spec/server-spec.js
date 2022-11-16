7bfeed7b3c060e8929162c21e6a979898c175f8a
Ariel <60643935+Waittill@users.noreply.github.com>
2022-11-15 21:30:29 -0800

    For Sharing Progress

--- server/spec/server-spec.js
+++ server/spec/server-spec.js
@@ -8,15 +8,15 @@ const API_URL = 'http://127.0.0.1:3000/classes';
 
 describe('Persistent Node Chat Server', () => {
   const dbConnection = mysql.createConnection({
-    user: 'student',
-    password: 'student',
+    user: 'root',
+    password: '',
     database: 'chat',
   });
 
   beforeAll((done) => {
     dbConnection.connect();
 
-       const tablename = ''; // TODO: fill this out
+    const tablename = 'messages'; // TODO: fill this out
 
     /* Empty the db table before all tests so that multiple tests
      * (or repeated runs of the tests)  will not fail when they should be passing
@@ -43,8 +43,9 @@ describe('Persistent Node Chat Server', () => {
 
         /* TODO: You might have to change this test to get all the data from
          * your message table, since this is schema-dependent. */
-        const queryString = 'SELECT * FROM messages';
-        const queryArgs = [];
+
+        const queryString = 'SELECT * FROM `messages` WHERE `user` = ? AND `messageText` = ? AND `roomname` = ?';
+        const queryArgs = [username, message, roomname];
 
         dbConnection.query(queryString, queryArgs, (err, results) => {
           if (err) {
@@ -54,7 +55,8 @@ describe('Persistent Node Chat Server', () => {
           expect(results.length).toEqual(1);
 
           // TODO: If you don't have a column named text, change this test.
-          expect(results[0].text).toEqual(message);
+          console.log('results for test', results);
+          expect(results[0].messageText).toEqual(message);
           done();
         });
       })
@@ -65,8 +67,8 @@ describe('Persistent Node Chat Server', () => {
 
   it('Should output all messages from the DB', (done) => {
     // Let's insert a message into the db
-       const queryString = '';
-       const queryArgs = [];
+    const queryString = 'SELECT * FROM messages';
+    const queryArgs = [];
     /* TODO: The exact query string and query args to use here
      * depend on the schema you design, so I'll leave them up to you. */
     dbConnection.query(queryString, queryArgs, (err) => {
@@ -78,7 +80,7 @@ describe('Persistent Node Chat Server', () => {
       axios.get(`${API_URL}/messages`)
         .then((response) => {
           const messageLog = response.data;
-          expect(messageLog[0].text).toEqual(message);
+          expect(messageLog[0].messageText).toEqual(message);
           expect(messageLog[0].roomname).toEqual(roomname);
           done();
         })
