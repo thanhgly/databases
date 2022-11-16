c36d695eed97392580d990d61d47701da9b1f7f7
Ariel <60643935+Waittill@users.noreply.github.com>
2022-11-15 20:53:44 -0800

    Complete designing multi-table schema

--- server/schema.sql
+++ server/schema.sql
@@ -4,7 +4,18 @@ USE chat;
 
 CREATE TABLE messages (
   /* Describe your table here.*/
+
+  /* The id of the message. */
+  id INT NOT NULL AUTO_INCREMENT,
+  roomname VARCHAR(255) NOT NULL,
+  user VARCHAR(255) NOT NULL,
+  messageText VARCHAR(255) NOT NULL,
+  PRIMARY KEY (id)
+
 );
+
+
+
 
 /* Create other tables and define schemas for them here! */
 
